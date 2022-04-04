6326c03fda897eece46eafeaff8577f25e6d2215
CodexpathCommunity <codexpath3@gmail.com>
2022-04-04 14:19:30 +0100

    added controllers and validate requests

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import { createUserHandler } from "../controller/user.controller";
 import validateResource from "../middlewares/validateResource";
 import { createUserSchema } from "../shema/user.schema";
 
 const router = express.Router();
 
-router.post("/user", validateResource(createUserSchema), (req, res) =>
-  res.sendStatus(200)
-);
+router.post("/user", validateResource(createUserSchema), createUserHandler);
 
 export default router;
